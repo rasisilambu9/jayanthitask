0ff5e0bcebd8e42aa2dd84076eba76e324969bcb
Silambarasan <rasisilambu9@gmail.com>
2021-12-09 22:15:01 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { LendComponent } from './lend/lend.component';
+import { ReturnComponent } from './return/return.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'lend',
+    component:LendComponent
+  },
+  {
+    path:'return',
+    component:ReturnComponent
+  },
+  {
+    path:'book',
+    component:BookComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { LendComponent } from './lend/lend.component';
+import { ReturnComponent } from './return/return.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LendComponent,
+    ReturnComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
