43bd47abb180a9844165153d1d50958202ab92cf
Silambarasan <rasisilambu9@gmail.com>
2021-12-10 19:11:32 +0530

    design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,20 @@ import { LendComponent } from './lend/lend.component';
 import { ReturnComponent } from './return/return.component';
 import { BookComponent } from './book/book.component';
 import { HttpClientModule } from '@angular/common/http';
-// materials
+// materials -button, tables, toolbar
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
 import { MatToolbarModule } from  '@angular/material/toolbar';
+
+// card
+import {MatCardModule} from '@angular/material/card';
+
+// form fields
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -35,9 +42,12 @@ import { MatToolbarModule } from  '@angular/material/toolbar';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatFormFieldModule,
     MatButtonModule,
     MatTableModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
