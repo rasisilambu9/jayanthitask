ac3311726a8642cf850ec711d05d8d3e6aac907d
Silambarasan <rasisilambu9@gmail.com>
2021-12-10 18:38:26 +0530

    books completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// routing
 import { AppRoutingModule } from './app-routing.module';
+// application
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+
+// pages
 import { LendComponent } from './lend/lend.component';
 import { ReturnComponent } from './return/return.component';
 import { BookComponent } from './book/book.component';
 import { HttpClientModule } from '@angular/common/http';
+// materials
+import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatTableModule} from '@angular/material/table';
 import { MatToolbarModule } from  '@angular/material/toolbar';
 
 
@@ -32,6 +36,7 @@ import { MatToolbarModule } from  '@angular/material/toolbar';
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
+    MatTableModule,
     MatIconModule
   ],
   providers: [],
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: string;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'C Programming', weight: 'Reema', symbol: 'H'},
+  {position: 2, name: 'Java', weight: 'Ramsri', symbol: 'He'},
+  {position: 3, name: 'OS', weight: 'Govind', symbol: 'Li'},
+  {position: 4, name: 'Maths', weight: 'Ramesh', symbol: 'Be'},
+  {position: 5, name: 'Physics', weight: 'Prakhar', symbol: 'B'},
+  {position: 6, name: 'Angular JS', weight: 'Mugunthan', symbol: 'C'},
+  {position: 7, name: 'React JS', weight: 'Kunal', symbol: 'N'},
+  {position: 8, name: 'Web development', weight: 'savita', symbol: 'O'},
+  {position: 9, name: 'Cyber Security', weight: 'sathya', symbol: 'F'},
+  {position: 10, name: 'AWS', weight:'Sanya', symbol: 'Ne'},
+];
+
+
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
@@ -11,5 +32,7 @@ export class BookComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
 }
